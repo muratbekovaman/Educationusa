d15c3ebcaa0316aed6467e1a4ca41387697f7c75
Aman <muratbekov.aman1@gmail.com>
2024-02-15 20:30:50 +0600

    changes in middleware files

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,19 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware(
+  
   {
-
+  debug: true,
   publicRoutes: ["/", 
   "/events/:id",
-  "/api/webhook/clerk",
-  "/api/webhook/stripe",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
   "/api/webhook/uploadthing"
 ],
 ignoredRoutes: [
-"/api/webhook/clerk",
-"/api/webhook/stripe",
-"/api/webhook/uploadthing"
+"/api/webhooks/clerk",
+"/api/webhooks/stripe",
+"/api/webhooks/uploadthing"
 ],
 
 });
